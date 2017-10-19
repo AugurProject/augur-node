716973409f7d2978ea83203fd104ca9ce5811b70
Paul Gebheim <paul@onetaste.us>
2017-10-19 11:23:11 -0700

    Use correct numeric types in the schema builder

--- src/migrations/20171015155157_reports_designated.ts
+++ src/migrations/20171015155157_reports_designated.ts
@@ -2,12 +2,12 @@ import * as Knex from "knex";
 
 exports.up = async (knex: Knex): Promise<any> => {
   return knex.schema.dropTableIfExists("reports_designated").then( (): PromiseLike<any> => {
-    return  knex.schema.createTable("reports_designated", (table: Knex.CreateTableBuilder) => {
+    return  knex.schema.createTable("reports_designated", (table: Knex.CreateTableBuilder): void => {
       table.increments("reportDesignatedID");
       table.string("marketID", 66).notNullable();
       // TODO: discuss correct datatype
       for (let i: number = 0; i <= 14; i++ ) {
-        table.integer("payout" + i).nullable();
+        table.specificType(`payout${i}`, "NUMERIC").nullable();
       }
       table.integer("isInvalid");
     });
