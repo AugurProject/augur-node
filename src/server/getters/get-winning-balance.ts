d3fab2c0f59d2d9c95b99c84df0fc51df6846212
Scott Bigelow <epheph@gmail.com>
2018-05-08 21:48:33 -0700

    calculate winnings

--- src/server/getters/get-winning-balance.ts
+++ src/server/getters/get-winning-balance.ts
@@ -1,23 +1,30 @@
 import * as Knex from "knex";
 import * as _ from "lodash";
-import { Address, UIMarketCreatorFee, ReportingState} from "../../types";
-import { getMarketsWithReportingState } from "./database";
+import { BigNumber } from "bignumber.js";
+import { Address, ReportingState, PayoutRow } from "../../types";
+import { getMarketsWithReportingState, groupByAndSum } from "./database";
 import Augur from "augur.js";
-import { ZERO } from "../../constants";
 
-interface MarketCreatorFeesRow {
+
+interface WinningPayoutRows extends PayoutRow<BigNumber> {
   marketId: Address;
   reportingState: ReportingState;
-  marketCreatorFeesBalance: BigNumber;
   balance: BigNumber;
+  numTicks: BigNumber;
+  outcome: number;
 }
 
-export function getWinningBalance(db: Knex, augur: Augur, marketIds: Array<Address>, account: Address, callback: (err: Error|null, result?: any) => void): void {
+export interface MarketWinnings {
+  marketId: Address;
+  winnings: BigNumber;
+}
+
+export function getWinningBalance(db: Knex, augur: Augur, marketIds: Array<Address>, account: Address, callback: (err: Error|null, result?: Array<MarketWinnings>) => void): void {
   if (marketIds == null) return callback(new Error("must include marketIds parameter"));
   if (account == null) return callback(new Error("must include account parameter"));
-  const marketsQuery: Knex.QueryBuilder = getMarketsWithReportingState(db, ["markets.marketId", "markets.numTicks", "balances.balance", "balances.owner"]);
-  // marketsQuery.whereIn("markets.marketId", marketIds);
-  // marketsQuery.whereIn("reportingState", [ReportingState.FINALIZED, ReportingState.AWAITING_FINALIZATION])
+  const marketsQuery: Knex.QueryBuilder = getMarketsWithReportingState(db, ["markets.marketId", "markets.numTicks", "balances.balance", "balances.owner", "shareTokens.outcome", "payouts.*"]);
+  marketsQuery.whereIn("markets.marketId", marketIds);
+  marketsQuery.whereIn("reportingState", [ReportingState.FINALIZED, ReportingState.AWAITING_FINALIZATION])
   marketsQuery.join("tokens AS shareTokens", function () {
     this
       .on("shareTokens.marketId", "markets.marketId")
@@ -26,16 +33,22 @@ export function getWinningBalance(db: Knex, augur: Augur, marketIds: Array<Addre
   marketsQuery.join("balances", function () {
     this
       .on("balances.token", "shareTokens.contractAddress")
-      // .andOn("balances.owner", db.raw("?", account));
+      .andOn("balances.owner", db.raw("?", account));
   });
   marketsQuery.join("payouts", function () {
     this
       .on("payouts.marketId", "markets.marketId")
-      // .andOn("payouts.winning", db.raw("1"));
-    // .andOn("balances.owner", db.raw("?", account));
+      .andOn("payouts.winning", db.raw("1"));
   });
-  marketsQuery.asCallback(( err: Error|null, marketCreatorFeeRows: Array<MarketCreatorFeesRow>): void => {
+  marketsQuery.asCallback(( err: Error|null, winningPayoutRows: Array<WinningPayoutRows>): void => {
     if (err != null) return callback(err);
-    callback(null, marketCreatorFeeRows);
+    const calculatedWinnings = _.map(winningPayoutRows, (winningPayoutRow) => {
+      const payoutKey = `payout${winningPayoutRow.outcome}` as keyof PayoutRow<BigNumber>;
+      const payout = winningPayoutRow[payoutKey] as BigNumber;
+      const winnings: BigNumber = payout.dividedBy(winningPayoutRow.numTicks).times(winningPayoutRow.balance);
+      return {marketId: winningPayoutRow.marketId, winnings: winnings.decimalPlaces(0, BigNumber.ROUND_DOWN) };
+    });
+    console.log(calculatedWinnings);
+    callback(null, groupByAndSum(calculatedWinnings, ["marketId"], ["winnings"]));
   });
 }
