8a9000ed23186e24ae02420edc98a00dca9907e9
Tom Haile <bradford.haile@gmail.com>
2018-07-26 14:12:14 -0500

    Update get-markets-search.ts
    set return type

--- src/server/getters/get-markets-search.ts
+++ src/server/getters/get-markets-search.ts
@@ -3,7 +3,7 @@ import { Address, MarketsContractAddressRow } from "../../types";
 import { queryModifier, getMarketsWithReportingState } from "./database";
 
 // Returning marketIds should likely be more generalized, since it is a single line change for most getters (awaiting reporting, by user, etc)
-export function getMarketsSearch(db: Knex, universe: Address, search: string, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err?: Error|null, result?: any) => void): void {
+export function getMarketsSearch(db: Knex, universe: Address, search: string, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err?: Error|null, result?: Array<string>) => void): void {
   if (universe == null) return callback(new Error("Must provide universe"));
   if (search == null) return callback(new Error("Must provide search terms"));
   const queryMarkets = getMarketsWithReportingState(db, ["markets.marketId", "marketStateBlock.timestamp as reportingStateUpdatedOn"]);
