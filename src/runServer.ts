1aea5afe44553619b463f4520870db0b0c78204f
Tom Haile <bradford.haile@gmail.com>
2018-07-27 14:47:37 -0500

    add retry logic to run server for users that run augur-node commandline

--- src/runServer.ts
+++ src/runServer.ts
@@ -5,27 +5,39 @@ import { logger } from "./utils/logger";
 
 const networkName = process.argv[2] || "environment";
 const databaseDir = process.env.AUGUR_DATABASE_DIR;
+const maxRetries = process.env.MAX_RETRIES || "0";
 const propagationDelayWaitMillis = process.env.DELAY_WAIT_MILLIS;
 const networkConfig = NetworkConfiguration.create(networkName);
 const augur: Augur = new Augur();
 
 let config = networkConfig;
+if (maxRetries) config = Object.assign({}, config, { maxRetries });
 if (propagationDelayWaitMillis) config = Object.assign({}, config, { propagationDelayWaitMillis });
+const retries: number = parseInt(maxRetries, 10);
 
-console.log("networkConfig", JSON.stringify(config));
+function start(retries: number, augur: Augur, config: any, databaseDir: any) {
+  const augurNodeController = new AugurNodeController(augur, config, databaseDir);
 
-const augurNodeController = new AugurNodeController(augur, config, databaseDir);
+  augur.rpc.setDebugOptions({ broadcast: false });
+  augur.events.nodes.ethereum.on("disconnect", (event) => {
+    logger.warn("Disconnected from Ethereum node", (event || {}).reason);
+  });
 
-augur.rpc.setDebugOptions({ broadcast: false });
-augur.events.nodes.ethereum.on("disconnect", (event) => {
-  logger.warn("Disconnected from Ethereum node", event);
-  augurNodeController.shutdown();
-  throw new Error("Disconnected from Ethereum node");
-});
+  augur.events.nodes.ethereum.on("reconnect", () => {
+    logger.warn("Reconnect to Ethereum node");
+  });
 
-const errorCallback = (err: any) => {
-  logger.error("Fatal Error:", err);
-  process.exit(1);
-};
+  function errorCatch(err: any) {
+    if (retries > 0) {
+      retries--;
+      augurNodeController.shutdown();
+      setTimeout(start(retries, augur, config, databaseDir), 1000);
+    } else {
+      process.exit(1);
+    }
+  }
 
-augurNodeController.start(errorCallback).catch(errorCallback);
+  augurNodeController.start(errorCatch).catch(errorCatch);
+}
+
+start(retries, augur, config, databaseDir);
