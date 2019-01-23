230d7aeca360b22d3a181f298183fd00c8ed2a40
nuevoalex <achap5dk@gmail.com>
2019-01-22 16:08:12 -0800

    fix for ordering when getting latest rows from pl timeseries

--- test/unit/blockchain/log-processors/tokens-transferred.js
+++ test/unit/blockchain/log-processors/tokens-transferred.js
@@ -172,6 +172,7 @@ describe("blockchain/log-processors/tokens-transferred", () => {
         tokenType: 1,
         market: "0x0000000000000000000000000000000000000002",
         blockNumber: 1400101,
+        transactionIndex: 1,
       },
       augur: {
         contracts: {
