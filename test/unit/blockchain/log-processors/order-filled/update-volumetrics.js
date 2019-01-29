ce769d3e7c93a8bbdcc514a05177f78610a98634
Ryan Berckmans <ryanberckmans@gmail.com>
2019-01-28 18:03:31 -0600

    Fix volume calculation (#805)
    Fixes AugurProject/augur#904 and details in that ticket.

--- test/unit/blockchain/log-processors/order-filled/update-volumetrics.js
+++ test/unit/blockchain/log-processors/order-filled/update-volumetrics.js
@@ -6,18 +6,21 @@ test("volumeForTrade", () => {
   const testData = [
     {
       name: "general test case where every input contributes to the output",
-      numTicks: new BigNumber("12", 10),
+      marketMinPrice: new BigNumber("-5", 10),
+      marketMaxPrice: new BigNumber("50", 10),
       numCreatorTokens: new BigNumber("17", 10),
       numCreatorShares: new BigNumber("19", 10),
       numFillerTokens: new BigNumber("31", 10),
       numFillerShares: new BigNumber("70", 10),
-      expectedVolume: new BigNumber("276", 10),
+      expectedVolume: new BigNumber("1093", 10),
     },
     // append test cases here as needed
   ];
 
   testData.forEach(td => {
-    expect(volumeForTrade(td.numTicks, {
+    expect(volumeForTrade({
+      marketMinPrice: td.marketMinPrice,
+      marketMaxPrice: td.marketMaxPrice,
       numCreatorTokens: td.numCreatorTokens,
       numCreatorShares: td.numCreatorShares,
       numFillerTokens: td.numFillerTokens,
