d4e6f1a6d7820ca61fb49d65320892eb86b1acdb
Filip Pyrek <PyrekFilip@gmail.com>
2019-02-03 20:46:47 +0100

    Fix profit_loss_timeseries to be compatible with Postgres

--- src/migrations/20181009111121_profit_loss_timeseries.ts
+++ src/migrations/20181009111121_profit_loss_timeseries.ts
@@ -5,11 +5,11 @@ exports.up = async (knex: Knex): Promise<any> => {
     return knex.schema.createTable("profit_loss_timeseries", (table: Knex.CreateTableBuilder): void => {
       table.string("account", 42).notNullable();
       table.string("marketId", 42).notNullable();
-      table.specificType("outcome", "integer NOT NULL CONSTRAINT nonnegativeOutcome CHECK (outcome >= 0)");
+      table.specificType("outcome", "integer NOT NULL CONSTRAINT nonnegativeOutcome CHECK (\"outcome\" >= 0)");
       table.string("transactionHash", 66).notNullable();
-      table.specificType("moneySpent", "varchar(255) NOT NULL DEFAULT '0' CONSTRAINT nonnegativemoneySpent CHECK (ltrim(moneySpent, '-') = moneySpent)");
-      table.specificType("numOwned", "varchar(255) NOT NULL DEFAULT '0' CONSTRAINT nonnegativeNumOwned CHECK (ltrim(numOwned, '-') = numOwned)");
-      table.specificType("numEscrowed", "varchar(255) NOT NULL DEFAULT '0' CONSTRAINT nonnegativeNumEscrowed CHECK (ltrim(numEscrowed, '-') = numEscrowed)");
+      table.specificType("moneySpent", "varchar(255) NOT NULL DEFAULT '0' CONSTRAINT nonnegativemoneySpent CHECK (ltrim(\"moneySpent\", '-') = \"moneySpent\")");
+      table.specificType("numOwned", "varchar(255) NOT NULL DEFAULT '0' CONSTRAINT nonnegativeNumOwned CHECK (ltrim(\"numOwned\", '-') = \"numOwned\")");
+      table.specificType("numEscrowed", "varchar(255) NOT NULL DEFAULT '0' CONSTRAINT nonnegativeNumEscrowed CHECK (ltrim(\"numEscrowed\", '-') = \"numEscrowed\")");
       table.string("profit", 255);
       table.specificType("timestamp", "integer NOT NULL CONSTRAINT nonnegativeTimestamp CHECK (\"timestamp\" >= 0)");
       table.specificType("blockTransactionIndex", "integer NOT NULL CONSTRAINT nonnegativeBlockTransactionIndex CHECK (\"blockTransactionIndex\" >= 0)");
