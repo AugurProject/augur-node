b89a86cfdf21f7e9ba08aa4c2741197a07f8f425
Ryan Berckmans <ryanberckmans@gmail.com>
2019-03-11 01:14:55 -0500

    WIP iterating on dimension-quantity

--- src/utils/dimension-quantity.ts
+++ src/utils/dimension-quantity.ts
@@ -38,14 +38,22 @@ function isEqual(a: DimensionVector, b: DimensionVector): boolean {
 
 abstract class Quantity<T extends Quantity<T>> {
   // TODO an idea is to carry a history of dimension changes through operations so that when a dynamic dimension/type check fails, can give an explanation of where it came from
+  public readonly derivedConstructor: QuantityConstructor<T>;
   public readonly magnitude: BigNumber;
   public readonly dimension: DimensionVector;
-  public readonly derivedConstructor: QuantityConstructor<T>;
+  public readonly sign: -1 | 1;
   protected constructor(derivedConstructor: QuantityConstructor<T>, magnitude: BigNumber, dimension: DimensionVector) {
     // TODO allow magnitude to be a number for convenience
     this.derivedConstructor = derivedConstructor;
     this.magnitude = magnitude;
     this.dimension = dimension;
+    if (magnitude.s === -1) {
+      this.sign = -1;
+    } else if (magnitude.s === 1) {
+      this.sign = 1;
+    } else {
+      throw new Error(`expected BigNumber.s to be -1 or 1, was ${this.magnitude.s}`);
+    }
   }
   public multipliedBy(other: Percent): T;
   public multipliedBy(other: Scalar): T extends (Percent) ? Scalar : T;
@@ -65,9 +73,31 @@ abstract class Quantity<T extends Quantity<T>> {
     if (other instanceof Scalar || other instanceof Percent) {
       return new this.derivedConstructor(newMagnitude);
     }
-    const c = Object.assign({}, this.dimension);
-    Dimensions.forEach((u) => c[u] += other.dimension[u]);
-    return new UnverifiedQuantity(newMagnitude, c);
+    const newDimension = Object.assign({}, this.dimension);
+    Dimensions.forEach((u) => newDimension[u] += other.dimension[u]);
+    return new UnverifiedQuantity(newMagnitude, newDimension);
+  }
+  public dividedBy(other: Percent): T;
+  public dividedBy(other: Scalar): T extends (Percent) ? Scalar : T;
+  public dividedBy<B extends Quantity<B>>(other: B): T extends (Scalar | Percent) ? B : UnverifiedQuantity;
+  public dividedBy<B extends Quantity<B>>(other: B): B | T | UnverifiedQuantity {
+    const newMagnitude = this.magnitude.dividedBy(other.magnitude);
+    // TODO doc lack of symmtery between Scalar/Percent is why we need these special cases. Scalar is stronger than Percent because Scalar*Percent=Scalar
+    if (this instanceof Scalar && other instanceof Percent) {
+      return new this.derivedConstructor(newMagnitude);
+    }
+    if (other instanceof Scalar && this instanceof Percent) {
+      return new other.derivedConstructor(newMagnitude);
+    }
+    if (this instanceof Scalar || this instanceof Percent) {
+      return new other.derivedConstructor(newMagnitude);
+    }
+    if (other instanceof Scalar || other instanceof Percent) {
+      return new this.derivedConstructor(newMagnitude);
+    }
+    const newDimension = Object.assign({}, this.dimension);
+    Dimensions.forEach((u) => newDimension[u] -= other.dimension[u]);
+    return new UnverifiedQuantity(newMagnitude, newDimension);
   }
   public plus<B extends Quantity<B>>(other: B): T {
     if (!isEqual(this.dimension, other.dimension)) {
@@ -76,6 +106,28 @@ abstract class Quantity<T extends Quantity<T>> {
     const newMagnitude = this.magnitude.plus(other.magnitude);
     return new this.derivedConstructor(newMagnitude);
   }
+  public minus<B extends Quantity<B>>(other: B): T {
+    if (!isEqual(this.dimension, other.dimension)) {
+      throw new Error(`minus failed: expected dimensions to be equal, this=${this}, other=${other}`);
+    }
+    const newMagnitude = this.magnitude.minus(other.magnitude);
+    return new this.derivedConstructor(newMagnitude);
+  }
+  public isZero(): boolean {
+    return this.magnitude.isZero();
+  }
+  public abs(): T {
+    return new this.derivedConstructor(this.magnitude.abs());
+  }
+  // TODO generalized min(array)
+  public min<B extends Quantity<B>>(other: B): T {
+    if (!isEqual(this.dimension, other.dimension)) {
+      throw new Error(`min failed: expected dimensions to be equal, this=${this}, other=${other}`);
+    }
+    return new this.derivedConstructor(BigNumber.min(this.magnitude, other.magnitude));
+  }
+
+  // TODO there is a general pattern among some functions eg. plus/minus/min: ensure dimensions equal and then return derivedConstructor(some new magnitude); can probably simplify this code and it'd be worth it once more functions are added
 }
 
 interface QuantityConstructor<T extends Quantity<T>> {
@@ -96,34 +148,50 @@ class UnverifiedQuantity extends Quantity<UnverifiedQuantity> {
     }
     super(Copy, magnitude, dimension);
   }
-  // TODO consider renaming verify() to is() or shouldBe() or isIn()
-  public verify<T extends Quantity<T>>(vu: VerifiableQuantity<T>): T {
-    if (!isEqual(vu.sentinel.dimension, this.dimension)) {
-      throw new Error(`verify failed: dimensions not equal, expected=${vu}, actual=${this}`);
+  public expect<T extends Quantity<T>>(vq: VerifiableQuantity<T>): T {
+    if (!isEqual(vq.sentinel.dimension, this.dimension)) {
+      throw new Error(`expect failed: dimensions not equal, expected=${vq}, actual=${this}`);
     }
-    if (vu.isValidMagnitude !== undefined) {
-      const maybeErr = vu.isValidMagnitude(this.magnitude);
+    if (vq.isValidMagnitude !== undefined) {
+      const maybeErr = vq.isValidMagnitude(this.magnitude);
       if (maybeErr !== undefined) {
         // TODO wrap maybeErr with dimension context
         throw maybeErr;
       }
     }
-    return new vu(this.magnitude);
+    return new vq(this.magnitude);
   }
 }
 
+// ************************************************************************
+// **** specific units below here; a lot of this could be generated code
+
+// TODO rename sentinel to ZERO
+
+export function scalar(magnitude: number | BigNumber): Scalar {
+  if (typeof magnitude === "number") {
+    return new Scalar(new BigNumber(magnitude));
+  }
+  return new Scalar(magnitude);
+}
+const ScalarUnitDimension: DimensionVector = Object.freeze({
+  tokens: 0,
+  shares: 0,
+});
 export class Scalar extends Quantity<Scalar> {
   public static sentinel: Scalar = new Scalar(ZERO);
-  protected static readonly unitDimension: DimensionVector = {
-    tokens: 0,
-    shares: 0,
-  };
   public scalar: true; // this unique field makes this unit disjoint with other units so that you can't `a: Tokens = new Scalar()`
   constructor(magnitude: BigNumber) {
-    super(Scalar, magnitude, Scalar.unitDimension);
+    super(Scalar, magnitude, ScalarUnitDimension);
   }
 }
 
+export function percent(magnitude: number | BigNumber): Percent {
+  if (typeof magnitude === "number") {
+    return new Percent(new BigNumber(magnitude));
+  }
+  return new Percent(magnitude);
+}
 export class Percent extends Quantity<Percent> {
   public static sentinel: Percent = new Percent(ZERO);
   public static isValidMagnitude(magnitude: BigNumber): undefined | Error {
@@ -132,37 +200,63 @@ export class Percent extends Quantity<Percent> {
     }
     return;
   }
-  protected static readonly unitDimension: DimensionVector = {
-    tokens: 0,
-    shares: 0,
-  };
   public percent: true; // this unique field makes this unit disjoint with other units so that you can't `a: Tokens = new Percent2()`
   constructor(magnitude: BigNumber) {
-    super(Percent, magnitude, Percent.unitDimension);
+    super(Percent, magnitude, ScalarUnitDimension);
   }
 }
 
-class Tokens extends Quantity<Tokens> {
+export function tokens(magnitude: number | BigNumber): Tokens {
+  if (typeof magnitude === "number") {
+    return new Tokens(new BigNumber(magnitude));
+  }
+  return new Tokens(magnitude);
+}
+const TokensUnitDimension: DimensionVector = Object.freeze({
+  tokens: 1,
+  shares: 0,
+});
+export class Tokens extends Quantity<Tokens> {
   public static sentinel: Tokens = new Tokens(ZERO);
-  protected static readonly unitDimension: DimensionVector = {
-    tokens: 1,
-    shares: 0,
-  };
   public tokens: true; // this unique field makes this unit disjoint with other units so that you can't `a: Tokens = new Scalar()`
   constructor(magnitude: BigNumber) {
-    super(Tokens, magnitude, Tokens.unitDimension);
+    super(Tokens, magnitude, TokensUnitDimension);
   }
 }
 
-class Shares extends Quantity<Shares> {
+export function shares(magnitude: number | BigNumber): Shares {
+  if (typeof magnitude === "number") {
+    return new Shares(new BigNumber(magnitude));
+  }
+  return new Shares(magnitude);
+}
+const SharesUnitDimension: DimensionVector = Object.freeze({
+  tokens: 0,
+  shares: 1,
+});
+export class Shares extends Quantity<Shares> {
   public static sentinel: Shares = new Shares(ZERO);
-  protected static readonly unitDimension: DimensionVector = {
-    tokens: 0,
-    shares: 1,
-  };
   public shares: true; // this unique field makes this unit disjoint with other units so that you can't `a: Shares = new Scalar()`
   constructor(magnitude: BigNumber) {
-    super(Shares, magnitude, Shares.unitDimension);
+    super(Shares, magnitude, SharesUnitDimension);
+  }
+}
+
+export function price(magnitude: number | BigNumber): Price {
+  if (typeof magnitude === "number") {
+    return new Price(new BigNumber(magnitude));
+  }
+  return new Price(magnitude);
+}
+const PriceUnitDimension: DimensionVector = Object.freeze({
+  tokens: 1,
+  shares: -1,
+});
+export class Price extends Quantity<Price> {
+  public static sentinel: Price = new Price(ZERO);
+  public price: true; // this unique field makes this unit disjoint with other units so that you can't `a: Price = new Scalar()`
+  constructor(magnitude: BigNumber) {
+    super(Price, magnitude, PriceUnitDimension);
   }
 }
 
@@ -192,4 +286,9 @@ const uq2 = new UnverifiedQuantity(ONE, {
   shares: 0,
 });
 
-const t8: Tokens = uq2.verify(Tokens);
+const t8: Tokens = uq2.expect(Tokens);
+
+const pr: Price = new UnverifiedQuantity(ONE, {
+  tokens: 1,
+  shares: -1,
+}).expect(Price);
