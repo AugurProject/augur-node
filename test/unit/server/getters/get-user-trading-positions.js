9dbfb47f4dbe8c2524b531f6ad9c2409a31861bd
Ryan Berckmans <ryanberckmans@gmail.com>
2019-03-29 18:58:29 -0500

    Update tests for getUserTradingPositions()

--- test/unit/server/getters/get-user-trading-positions.js
+++ test/unit/server/getters/get-user-trading-positions.js
@@ -64,6 +64,7 @@ describe("server/getters/get-user-trading-positions#Binary-1", () => {
     const {
       frozenFundsTotal,
       tradingPositions,
+      tradingPositionsPerMarket,
     } = await getUserTradingPositions({
       universe,
       account,
@@ -80,6 +81,28 @@ describe("server/getters/get-user-trading-positions#Binary-1", () => {
     expect(tradingPositions[0].unrealized).toEqual("0.49");
     expect(tradingPositions[0].realized).toEqual("0.21");
     expect(tradingPositions[0].frozenFunds).toEqual("2.45");
+    expect(tradingPositions[0].unrealizedCost).toEqual("2.45");
+    expect(tradingPositions[0].unrealizedRevenue).toEqual("2.94");
+    expect(tradingPositions[0].unrealizedPercent).toEqual("0.2");
+    expect(tradingPositions[0].realizedCost).toEqual("1.05");
+    expect(tradingPositions[0].realizedPercent).toEqual("0.2");
+    expect(tradingPositions[0].totalCost).toEqual("3.5");
+    expect(tradingPositions[0].total).toEqual("0.7");
+    expect(tradingPositions[0].totalPercent).toEqual("0.2");
+
+    const market = tradingPositionsPerMarket[marketId];
+    expect(market.marketId).toEqual(marketId);
+    expect(market.unrealizedCost).toEqual("2.45");
+    expect(market.unrealizedRevenue).toEqual("2.94");
+    expect(market.unrealized).toEqual("0.49");
+    expect(market.unrealizedPercent).toEqual("0.2");
+    expect(market.realizedCost).toEqual("1.05");
+    expect(market.realized).toEqual("0.21");
+    expect(market.realizedPercent).toEqual("0.2");
+    expect(market.totalCost).toEqual("3.5");
+    expect(market.total).toEqual("0.7");
+    expect(market.totalPercent).toEqual("0.2");
+    expect(market.frozenFunds).toEqual("2.45");
 
     expect(frozenFundsTotal.frozenFunds).toEqual(bn(2.45).plus(validityBondSumInEth).toString());
   });
@@ -159,6 +182,7 @@ describe("server/getters/get-user-trading-positions#Binary-2", () => {
     const {
       frozenFundsTotal,
       tradingPositions,
+      tradingPositionsPerMarket,
     } = await getUserTradingPositions({
       universe,
       account,
@@ -175,6 +199,28 @@ describe("server/getters/get-user-trading-positions#Binary-2", () => {
     expect(tradingPositions[0].realized).toEqual("4.8785");
     expect(tradingPositions[0].unrealized).toEqual("1.4415");
     expect(tradingPositions[0].frozenFunds).toEqual("1.1085");
+    expect(tradingPositions[0].unrealizedCost).toEqual("1.1085");
+    expect(tradingPositions[0].unrealizedRevenue).toEqual("2.55");
+    expect(parseFloat(tradingPositions[0].unrealizedPercent)).toBeCloseTo(1.300406);
+    expect(tradingPositions[0].realizedCost).toEqual("7.3315");
+    expect(parseFloat(tradingPositions[0].realizedPercent)).toBeCloseTo(0.6654164);
+    expect(tradingPositions[0].totalCost).toEqual("8.44");
+    expect(tradingPositions[0].total).toEqual("6.32");
+    expect(parseFloat(tradingPositions[0].totalPercent)).toBeCloseTo(0.748815);
+
+    const market = tradingPositionsPerMarket[marketId];
+    expect(market.marketId).toEqual(marketId);
+    expect(market.unrealizedCost).toEqual("1.1085");
+    expect(market.unrealizedRevenue).toEqual("2.55");
+    expect(market.unrealized).toEqual("1.4415");
+    expect(parseFloat(market.unrealizedPercent)).toBeCloseTo(1.300406);
+    expect(market.realizedCost).toEqual("7.3315");
+    expect(market.realized).toEqual("4.8785");
+    expect(parseFloat(market.realizedPercent)).toBeCloseTo(0.6654164);
+    expect(market.totalCost).toEqual("8.44");
+    expect(market.total).toEqual("6.32");
+    expect(parseFloat(market.totalPercent)).toBeCloseTo(0.748815);
+    expect(market.frozenFunds).toEqual("1.1085");
 
     expect(frozenFundsTotal.frozenFunds).toEqual(bn(1.1085).plus(validityBondSumInEth).toString());
   });
@@ -266,6 +312,7 @@ describe("server/getters/get-user-trading-positions#Cat3-1", () => {
     const {
       frozenFundsTotal,
       tradingPositions,
+      tradingPositionsPerMarket,
     } = await getUserTradingPositions({
       universe,
       account,
@@ -286,18 +333,56 @@ describe("server/getters/get-user-trading-positions#Cat3-1", () => {
     expect(positionA.unrealized).toEqual("0");
     expect(positionA.realized).toEqual("0.3");
     expect(positionA.frozenFunds).toEqual("0");
+    expect(positionA.unrealizedCost).toEqual("0");
+    expect(positionA.unrealizedRevenue).toEqual("0");
+    expect(parseFloat(positionA.unrealizedPercent)).toEqual(0);
+    expect(positionA.realizedCost).toEqual("0.4");
+    expect(parseFloat(positionA.realizedPercent)).toBeCloseTo(0.75);
+    expect(positionA.totalCost).toEqual("0.4");
+    expect(positionA.total).toEqual("0.3");
+    expect(parseFloat(positionA.totalPercent)).toBeCloseTo(0.75);
 
     expect(positionB.netPosition).toEqual("-2");
     expect(positionB.averagePrice).toEqual("0.2");
     expect(positionB.unrealized).toEqual("0");
     expect(positionB.realized).toEqual("0");
     expect(positionB.frozenFunds).toEqual("1.6");
+    expect(positionB.unrealizedCost).toEqual("1.6");
+    expect(positionB.unrealizedRevenue).toEqual("1.6");
+    expect(parseFloat(positionB.unrealizedPercent)).toEqual(0);
+    expect(positionB.realizedCost).toEqual("0");
+    expect(parseFloat(positionB.realizedPercent)).toEqual(0);
+    expect(positionB.totalCost).toEqual("1.6");
+    expect(positionB.total).toEqual("0");
+    expect(parseFloat(positionB.totalPercent)).toEqual(0);
 
     expect(positionC.netPosition).toEqual("0.5");
     expect(positionC.averagePrice).toEqual("0.3");
     expect(positionC.unrealized).toEqual("0");
     expect(positionC.realized).toEqual("0");
     expect(positionC.frozenFunds).toEqual("0.15");
+    expect(positionC.unrealizedCost).toEqual("0.15");
+    expect(positionC.unrealizedRevenue).toEqual("0.15");
+    expect(parseFloat(positionC.unrealizedPercent)).toEqual(0);
+    expect(positionC.realizedCost).toEqual("0");
+    expect(parseFloat(positionC.realizedPercent)).toEqual(0);
+    expect(positionC.totalCost).toEqual("0.15");
+    expect(positionC.total).toEqual("0");
+    expect(parseFloat(positionC.totalPercent)).toEqual(0);
+
+    const market = tradingPositionsPerMarket[marketId];
+    expect(market.marketId).toEqual(marketId);
+    expect(market.unrealizedCost).toEqual("1.75");
+    expect(market.unrealizedRevenue).toEqual("1.75");
+    expect(market.unrealized).toEqual("0");
+    expect(parseFloat(market.unrealizedPercent)).toEqual(0);
+    expect(market.realizedCost).toEqual("0.4");
+    expect(market.realized).toEqual("0.3");
+    expect(parseFloat(market.realizedPercent)).toBeCloseTo(0.75);
+    expect(market.totalCost).toEqual("2.15");
+    expect(market.total).toEqual("0.3");
+    expect(parseFloat(market.totalPercent)).toBeCloseTo(0.139534);
+    expect(market.frozenFunds).toEqual("1.75");
 
     expect(frozenFundsTotal.frozenFunds.toString()).toEqual(bn(1.75).plus(validityBondSumInEth).toString());
   });
@@ -370,6 +455,7 @@ describe("server/getters/get-user-trading-positions#Cat3-2", () => {
     const {
       frozenFundsTotal,
       tradingPositions,
+      tradingPositionsPerMarket,
     } = await getUserTradingPositions({
       universe,
       account,
@@ -390,18 +476,56 @@ describe("server/getters/get-user-trading-positions#Cat3-2", () => {
     expect(positionA.unrealized).toEqual("0");
     expect(positionA.realized).toEqual("0");
     expect(positionA.frozenFunds).toEqual("3");
+    expect(positionA.unrealizedCost).toEqual("3");
+    expect(positionA.unrealizedRevenue).toEqual("3");
+    expect(parseFloat(positionA.unrealizedPercent)).toBeCloseTo(0);
+    expect(positionA.realizedCost).toEqual("0");
+    expect(parseFloat(positionA.realizedPercent)).toEqual(0);
+    expect(positionA.totalCost).toEqual("3");
+    expect(positionA.total).toEqual("0");
+    expect(parseFloat(positionA.totalPercent)).toEqual(0);
 
     expect(positionB.netPosition).toEqual("-3");
     expect(positionB.averagePrice).toEqual("0.35");
     expect(positionB.unrealized).toEqual("0");
     expect(positionB.realized).toEqual("0");
     expect(positionB.frozenFunds).toEqual("-1.05");
+    expect(positionB.unrealizedCost).toEqual("1.95");
+    expect(positionB.unrealizedRevenue).toEqual("1.95");
+    expect(parseFloat(positionB.unrealizedPercent)).toEqual(0);
+    expect(positionB.realizedCost).toEqual("0");
+    expect(parseFloat(positionB.realizedPercent)).toEqual(0);
+    expect(positionB.totalCost).toEqual("1.95");
+    expect(positionB.total).toEqual("0");
+    expect(parseFloat(positionB.totalPercent)).toEqual(0);
 
     expect(positionC.netPosition).toEqual("-2");
     expect(positionC.averagePrice).toEqual("0.3");
     expect(positionC.unrealized).toEqual("0.4");
     expect(positionC.realized).toEqual("1.6");
     expect(positionC.frozenFunds).toEqual("-0.6");
+    expect(positionC.unrealizedCost).toEqual("1.4");
+    expect(positionC.unrealizedRevenue).toEqual("1.8");
+    expect(parseFloat(positionC.unrealizedPercent)).toBeCloseTo(0.285714);
+    expect(positionC.realizedCost).toEqual("5.6");
+    expect(parseFloat(positionC.realizedPercent)).toBeCloseTo(0.285714);
+    expect(positionC.totalCost).toEqual("7");
+    expect(positionC.total).toEqual("2");
+    expect(parseFloat(positionC.totalPercent)).toBeCloseTo(0.285714);
+
+    const market = tradingPositionsPerMarket[marketId];
+    expect(market.marketId).toEqual(marketId);
+    expect(market.unrealizedCost).toEqual("6.35");
+    expect(market.unrealizedRevenue).toEqual("6.75");
+    expect(market.unrealized).toEqual("0.4");
+    expect(parseFloat(market.unrealizedPercent)).toBeCloseTo(0.06299);
+    expect(market.realizedCost).toEqual("5.6");
+    expect(market.realized).toEqual("1.6");
+    expect(parseFloat(market.realizedPercent)).toBeCloseTo(0.285714);
+    expect(market.totalCost).toEqual("11.95");
+    expect(market.total).toEqual("2");
+    expect(parseFloat(market.totalPercent)).toBeCloseTo(0.167364);
+    expect(market.frozenFunds).toEqual("1.35");
 
     expect(frozenFundsTotal.frozenFunds).toEqual(bn(1.35).plus(validityBondSumInEth).toString());
   });
@@ -492,6 +616,7 @@ describe("server/getters/get-user-trading-positions#Cat3-3", () => {
     const {
       frozenFundsTotal,
       tradingPositions,
+      tradingPositionsPerMarket,
     } = await getUserTradingPositions({
       universe,
       account,
@@ -512,18 +637,56 @@ describe("server/getters/get-user-trading-positions#Cat3-3", () => {
     expect(positionA.unrealized).toEqual("0");
     expect(positionA.realized).toEqual("-0.5");
     expect(positionA.frozenFunds).toEqual("2");
+    expect(positionA.unrealizedCost).toEqual("0");
+    expect(positionA.unrealizedRevenue).toEqual("0");
+    expect(parseFloat(positionA.unrealizedPercent)).toEqual(0);
+    expect(positionA.realizedCost).toEqual("1.5");
+    expect(parseFloat(positionA.realizedPercent)).toBeCloseTo(-0.33333);
+    expect(positionA.totalCost).toEqual("1.5");
+    expect(positionA.total).toEqual("-0.5");
+    expect(parseFloat(positionA.totalPercent)).toBeCloseTo(-0.33333);
 
     expect(positionB.netPosition).toEqual("12");
     expect(positionB.averagePrice).toEqual("0.1");
     expect(positionB.unrealized).toEqual("1.2");
     expect(positionB.realized).toEqual("1.3");
     expect(positionB.frozenFunds).toEqual("1.2");
+    expect(positionB.unrealizedCost).toEqual("1.2");
+    expect(positionB.unrealizedRevenue).toEqual("2.4");
+    expect(parseFloat(positionB.unrealizedPercent)).toEqual(1);
+    expect(positionB.realizedCost).toEqual("1.3");
+    expect(parseFloat(positionB.realizedPercent)).toEqual(1);
+    expect(positionB.totalCost).toEqual("2.5");
+    expect(positionB.total).toEqual("2.5");
+    expect(parseFloat(positionB.totalPercent)).toEqual(1);
 
     expect(positionC.netPosition).toEqual("2");
     expect(positionC.averagePrice).toEqual("0.6");
     expect(positionC.unrealized).toEqual("0.4");
     expect(positionC.realized).toEqual("0.6");
     expect(positionC.frozenFunds).toEqual("-0.8");
+    expect(positionC.unrealizedCost).toEqual("1.2");
+    expect(positionC.unrealizedRevenue).toEqual("1.6");
+    expect(parseFloat(positionC.unrealizedPercent)).toBeCloseTo(0.33333);
+    expect(positionC.realizedCost).toEqual("1.8");
+    expect(parseFloat(positionC.realizedPercent)).toBeCloseTo(0.33333);
+    expect(positionC.totalCost).toEqual("3");
+    expect(positionC.total).toEqual("1");
+    expect(parseFloat(positionC.totalPercent)).toBeCloseTo(0.33333);
+
+    const market = tradingPositionsPerMarket[marketId];
+    expect(market.marketId).toEqual(marketId);
+    expect(market.unrealizedCost).toEqual("2.4");
+    expect(market.unrealizedRevenue).toEqual("4");
+    expect(market.unrealized).toEqual("1.6");
+    expect(parseFloat(market.unrealizedPercent)).toBeCloseTo(0.66666);
+    expect(market.realizedCost).toEqual("4.6");
+    expect(market.realized).toEqual("1.4");
+    expect(parseFloat(market.realizedPercent)).toBeCloseTo(0.3043478);
+    expect(market.totalCost).toEqual("7");
+    expect(market.total).toEqual("3");
+    expect(parseFloat(market.totalPercent)).toBeCloseTo(0.428571);
+    expect(market.frozenFunds).toEqual("2.4");
 
     expect(frozenFundsTotal.frozenFunds).toEqual(bn(2.4).plus(validityBondSumInEth).toString());
   });
@@ -603,6 +766,7 @@ describe("server/getters/get-user-trading-positions#Scalar", () => {
     const {
       frozenFundsTotal,
       tradingPositions,
+      tradingPositionsPerMarket,
     } = await getUserTradingPositions({
       universe,
       account,
@@ -621,6 +785,28 @@ describe("server/getters/get-user-trading-positions#Scalar", () => {
     expect(tradingPositions[0].unrealized).toEqual("165");
     expect(tradingPositions[0].realized).toEqual("458");
     expect(tradingPositions[0].frozenFunds).toEqual("135");
+    expect(tradingPositions[0].unrealizedCost).toEqual("135");
+    expect(tradingPositions[0].unrealizedRevenue).toEqual("300");
+    expect(parseFloat(tradingPositions[0].unrealizedPercent)).toBeCloseTo(1.222222);
+    expect(tradingPositions[0].realizedCost).toEqual("1005");
+    expect(parseFloat(tradingPositions[0].realizedPercent)).toBeCloseTo(0.45572139);
+    expect(tradingPositions[0].totalCost).toEqual("1140");
+    expect(tradingPositions[0].total).toEqual("623");
+    expect(parseFloat(tradingPositions[0].totalPercent)).toBeCloseTo(0.54649123);
+
+    const market = tradingPositionsPerMarket[marketId];
+    expect(market.marketId).toEqual(marketId);
+    expect(market.unrealizedCost).toEqual("135");
+    expect(market.unrealizedRevenue).toEqual("300");
+    expect(market.unrealized).toEqual("165");
+    expect(parseFloat(market.unrealizedPercent)).toBeCloseTo(1.222222);
+    expect(market.realizedCost).toEqual("1005");
+    expect(market.realized).toEqual("458");
+    expect(parseFloat(market.realizedPercent)).toBeCloseTo(0.45572139);
+    expect(market.totalCost).toEqual("1140");
+    expect(market.total).toEqual("623");
+    expect(parseFloat(market.totalPercent)).toBeCloseTo(0.54649123);
+    expect(market.frozenFunds).toEqual("135");
 
     expect(frozenFundsTotal.frozenFunds).toEqual(bn(135).plus(validityBondSumInEth).toString());
   });
